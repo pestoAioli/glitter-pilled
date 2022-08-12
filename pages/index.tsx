fcbe18ff7cd7cce78a4f0d3c7beeabb4c0e274a1
Ricky <drickyriverab@gmail.com>
2022-08-12 16:17:40 -0700

    everything working in prod!!woohoo!!now im tweaking until its done. currently displays list of genres according to top 20 artists listened to, need to write  algorithm to generate score.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,40 @@
 import type { NextPage } from 'next'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import { useState } from 'react';
-
-
+import flatten from 'lodash.flatten';
 const Home: NextPage = () => {
   const { data: session } = useSession();
-  const [tracks, setTracks] = useState([]);
-
+  const [genres, setGenres] = useState([]);
+  const [title, setTitle] = useState('');
   const getMyTopTracks = async () => {
     const res = await fetch('api/tracks');
     const { items } = await res.json();
-    setTracks(items);
+    console.log(items);
+    const gen = items.map(item => {
+      return item.genres;
+    })
+    const flatGen = flatten(gen);
+    console.log(flatGen);
+    setGenres(flatGen);
+    setTitle('Here are your favorite genres you weirdo:')
   }
+
 
   if (session) {
     return (
       <div className='flex flex-col justify-center w-screen'>
         <p className='text-center'>Signed in</p>
         <hr />
-        <button className='text-orange-900 text-2xl' onClick={() => getMyTopTracks()}>Beware False Renunciates</button>
-        {tracks.map((track) => (
-          <div className='flex flex-col justify-between items-center' key={track.id}>
-            <h1 className='text-center'>{track.name}</h1>
-            <picture>
-              <img src={track.images[0].url} alt='picture' className='w-40' />
-            </picture>
+        <button className='text-pink-400 text-2xl' onClick={() => getMyTopTracks()}>Click here to run insanely goated algorithm</button>
+        <h1 className='text-center text-cyan-300 text-3xl'>{title}</h1>
+        {genres.map((genre, i) => (
+          < div className='flex flex-col justify-between items-center' key={i} >
+            <p className='text-center text-xl'>{genre}</p>
           </div>
-        ))}
+        ))
+        }
         <button onClick={() => signOut()}>Sign Out</button>
-      </div>
+      </div >
     )
   }
   return (
